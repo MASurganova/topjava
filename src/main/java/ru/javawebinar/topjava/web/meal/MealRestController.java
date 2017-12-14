58f2734e3b2bc293eafed6d546d216b78a432f6d
marina <masurganova@gmail.com>
2017-12-14 12:56:02 +0300

    HW06-1.3 (add localization)

--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -4,10 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import ru.javawebinar.topjava.AuthorizedUser;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealService;
@@ -15,15 +11,10 @@ import ru.javawebinar.topjava.to.MealWithExceed;
 import ru.javawebinar.topjava.util.DateTimeUtil;
 import ru.javawebinar.topjava.util.MealsUtil;
 
-import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.LocalTime;
-import java.time.temporal.ChronoUnit;
 import java.util.List;
 
-import static ru.javawebinar.topjava.util.DateTimeUtil.parseLocalDate;
-import static ru.javawebinar.topjava.util.DateTimeUtil.parseLocalTime;
 import static ru.javawebinar.topjava.util.ValidationUtil.assureIdConsistent;
 import static ru.javawebinar.topjava.util.ValidationUtil.checkNew;
 
@@ -37,56 +28,6 @@ public class MealRestController {
     public MealRestController(MealService service) {
         this.service = service;
     }
-
-    @GetMapping("/meals")
-    public String meals(Model model) {
-        model.addAttribute("meals", getAll());
-        return "meals";
-    }
-
-    @GetMapping("/meals/add")
-    public String create(Model model) {
-        model.addAttribute("meal", new Meal(LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES), "", 1000));
-        return "mealForm";
-    }
-
-    @GetMapping("/meals/update/{id}")
-    public String update(@PathVariable("id") int id, Model model) {
-        model.addAttribute("meal", get(id));
-        return "mealForm";
-    }
-
-    @GetMapping("/meals/delete/{id}")
-    public String remove(@PathVariable("id") int id) {
-        delete(id);
-        return "redirect:/meals";
-    }
-
-    @GetMapping("/meals/mealForm")
-    public String save(HttpServletRequest request) {
-        Meal meal = new Meal(
-                LocalDateTime.parse(request.getParameter("dateTime")),
-                request.getParameter("description"),
-                Integer.parseInt(request.getParameter("calories")));
-
-        if (request.getParameter("id").isEmpty()) {
-            create(meal);
-        } else {
-            update(meal, Integer.parseInt(request.getParameter("id")));
-        }
-        return "redirect:/meals";
-    }
-
-    @PostMapping("/meals")
-    public String filter(HttpServletRequest request, Model model) {
-        LocalDate startDate = parseLocalDate(request.getParameter("startDate"));
-        LocalDate endDate = parseLocalDate(request.getParameter("endDate"));
-        LocalTime startTime = parseLocalTime(request.getParameter("startTime"));
-        LocalTime endTime = parseLocalTime(request.getParameter("endTime"));
-        model.addAttribute("meals", getBetween(startDate, startTime, endDate, endTime));
-        return "meals";
-    }
-
 
     public Meal get(int id) {
         int userId = AuthorizedUser.id();
